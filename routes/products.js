697b12eeb2b3d3b2309af065638a63ff78c42d01
BIDIPTA05 <bidiptasaikia@gmail.com>
2023-03-18 00:47:28 +0530

    feat: order api config

--- routes/products.js
+++ routes/products.js
@@ -6,11 +6,25 @@ const mongoose = require("mongoose");
 
 //GET ROUTE
 router.get("/", (req, res, next) => {
-  Product.find()
+  Product.find().select("name price _id")
     .exec()
     .then((docs) => {
       message: "Handling GET requests to /products", console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id
+            }
+          };
+        }),
+      };
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
@@ -39,10 +53,19 @@ router.post("/", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: result,
+        message: "Created the product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result._id
+          }
+        },
       });
     })
+    
     .catch((err) => {
       console.log(err);
       res.status(500).json({
@@ -67,11 +90,19 @@ router.get("/:productId", (req, res, next) => {
   //     });
   // }
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then((doc) => {
       if (doc) {
         console.log(doc);
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            description: "GET_ALL_PRODUCTS",
+            url: "http://localhost:3000/products",
+          },
+        });
       } else {
         res
           .status(404)
@@ -101,7 +132,13 @@ router.patch("/:productId", (req, res, next) => {
     .exec()
     .then((result) => {
       console.log("Updated Successfully");
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" + id,
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -109,10 +146,6 @@ router.patch("/:productId", (req, res, next) => {
         error: err,
       });
     });
-
-  res.status(200).json({
-    message: "Updated product!",
-  });
 });
 
 
@@ -125,7 +158,14 @@ router.delete("/:productId", (req, res, next) => {
     .exec()
     .then((result) => {
       console.log("Deleted Successfully");
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product deleted",
+        request: {
+          type: "POST",
+          url: "http://localhost:3000/products",
+          body: { name: "String", price: "Number" },
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -134,9 +174,7 @@ router.delete("/:productId", (req, res, next) => {
       });
     });
 
-  res.status(200).json({
-    message: "Deleted product!",
-  });
+
 });
 
 
