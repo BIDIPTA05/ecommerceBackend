e9b80594cd3f2b22451feced632f459b52814bea
BIDIPTA05 <bidiptasaikia@gmail.com>
2023-03-18 14:19:07 +0530

    feat: populated the order api

--- routes/orders.js
+++ routes/orders.js
@@ -8,6 +8,7 @@ const Product = require("../models/product");
 router.get("/", (req, res, next) => {
   Order.find()
     .select("product quantity _id ")
+    .populate("product", "name price")
     .exec()
     .then((docs) => {
       res.status(200).json({
@@ -83,36 +84,40 @@ router.post("/", (req, res, next) => {
 
 
 
-
-
+//FETCH A SINGLE ORDER
 router.get("/:orderId", (req, res, next) => {
 
-  Order.findById(req.params.orderId).exec().then((order) => { 
-    if (!order) {
-      return res.status(404).json({
-        message: "Order not found",
-      });
-    }
-    res.status(200).json({
-      message: "Order details fetched",
-      order: order,
-      request: {
-        type: "GET",
-        url: "http://localhost:3000/orders",
+  Order.findById(req.params.orderId)
+
+    .populate("product", "name price")
+    .exec()
+    .then((order) => {
+      if (!order) {
+        return res.status(404).json({
+          message: "Order not found",
+        });
       }
+      res.status(200).json({
+        message: "Order details fetched",
+        order: order,
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/orders",
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).json({
-      error: err,
-    });
-  });
 });
 
 
 
 
-
+//DELETE AN ORDER
 router.delete("/:orderId", (req, res, next) => {
 
   Order.findByIdAndDelete({ _id: req.params.orderId })
